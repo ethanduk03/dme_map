fc572e3bfa783100194051d4b2572f02c9ab1d29
Ethan Duke <ethanaduke03@gmail.com>
2025-10-13 11:43:08 -0400

    Clean up heatmaps, explore one-hot encoding of supplies and specialities, add hover to map

--- ida.py
+++ ida.py
@@ -1,4 +1,5 @@
 # Import necessary libraries
+from sklearn.preprocessing import MultiLabelBinarizer
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -8,26 +9,44 @@ import streamlit as st
 suppliers = pd.read_excel("Data/MedicalEquipmentSuppliers.xls")
 states = pd.read_csv("Data/states.csv")
 
-st.title("Missing Data")
+st.title("Initial Data Analysis")
 
-col1, col2 = st.columns(2)
-with col1:
-    #fig1, ax1 = plt.subplots()
-    #sns.heatmap(suppliers.isna().transpose(), cmap = "magma", ax=ax1)
-    #st.write(fig1)
-    fig1 = plt.figure(figsize=(20,16))
-    ax1 = fig1.add_subplot(111)
-    sns.heatmap(suppliers.isna().transpose(), cmap = "magma", ax=ax1)
-    st.write(fig1)
 
-    
-with col2:
-    #missing_row_indices = states[states.isna().any(axis=1)].index.tolist()
-    #states.loc[missing_row_indices,]
-    #fig2, ax2 = plt.subplots()
-    #sns.heatmap(states.isna().transpose(), cmap = "magma", ax=ax2)
-    #st.write(fig2)
-    fig2 = plt.figure(figsize=(20,16))
-    ax2 = fig2.add_subplot(111)
-    sns.heatmap(states.isna().transpose(), cmap = "magma", ax=ax2)
-    st.write(fig2)
+st.header("Collection")
+
+
+st.header("Quality and Integrity")
+fig, axes = plt.subplots(1, 2, figsize=(35, 8))
+sns.heatmap(suppliers.isna().transpose(), cmap = "magma", ax=axes[0])
+sns.heatmap(states.isna().transpose(), cmap = "magma", ax=axes[1])
+st.write(fig)
+
+
+st.header("Structure and Context")
+split_suppliers = suppliers.copy(deep=True)
+split_suppliers = split_suppliers.dropna(subset=['specialitieslist', 'supplieslist']).reset_index(drop=True)
+split_suppliers["specialitieslist"] = split_suppliers["specialitieslist"].str.split('|')
+split_suppliers["supplieslist"] = split_suppliers["supplieslist"].str.split('|')
+
+specialities = set()
+for row in split_suppliers.itertuples():
+    row_specialities = row.specialitieslist
+    for supply in row_specialities:
+        specialities.add(supply)
+
+supplies = set()
+for row in split_suppliers.itertuples():
+    row_supplies = row.supplieslist
+    for supply in row_supplies:
+        supplies.add(supply)
+
+mlb = MultiLabelBinarizer()
+encoded = pd.DataFrame(
+    mlb.fit_transform(split_suppliers['specialitieslist']),
+    columns=mlb.classes_,
+    index=split_suppliers.index
+)
+df_encoded = pd.concat([split_suppliers, encoded], axis=1)
+
+
+st.header("Analysis Readiness Evaluation")
--- map.py
+++ map.py
@@ -7,6 +7,15 @@ suppliers = pd.read_excel("Data/MedicalEquipmentSuppliers.xls")
 accepted = suppliers[suppliers["acceptsassignement"] == True]
 
 st.title("Map of DME Suppliers Accepting Assignment")
-st.map(accepted, latitude="latitude", longitude="longitude")
+#st.map(accepted, latitude="latitude", longitude="longitude")
 #map = px.scatter_geo(accepted, lat="latitude", lon="longitude")
 #st.plotly_chart(map)
+
+map = px.scatter_map(suppliers,
+                        lat=suppliers.latitude,
+                        lon=suppliers.longitude,
+                        hover_name="practicename",
+                        hover_data=["practicecity", "practicestate"],
+                        color="acceptsassignement",
+                        zoom=1)
+st.plotly_chart(map)
